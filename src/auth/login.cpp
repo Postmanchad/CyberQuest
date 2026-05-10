a884c657062a45b303b43b62626da67ba0a42b55
Postmanchad <118473087+Postmanchad@users.noreply.github.com>
2026-05-09 20:42:04 -0400

    Update login.cpp

--- src/auth/login.cpp
+++ src/auth/login.cpp
@@ -4,23 +4,43 @@
 using namespace std;
 
 int main() {
+
     string username;
     string password;
 
+    const string correctUsername = "admin";
+    const string correctPassword = "Cyber123!";
+
+    int attempts = 0;
+    const int maxAttempts = 3;
+
     cout << "==== CyberQuest Login ====" << endl;
 
-    cout << "Enter username: ";
-    cin >> username;
+    while(attempts < maxAttempts) {
 
-    cout << "Enter password: ";
-    cin >> password;
+        cout << "Enter username: ";
+        cin >> username;
 
-    if(username == "admin" && password == "Cyber123!") {
-        cout << "Login successful!" << endl;
-    }
-    else {
+        cout << "Enter password: ";
+        cin >> password;
+
+        if(username == correctUsername &&
+           password == correctPassword) {
+
+            cout << "Login successful!" << endl;
+            return 0;
+        }
+
+        attempts++;
+
         cout << "Invalid username or password." << endl;
+        cout << "Attempts remaining: "
+             << maxAttempts - attempts
+             << endl;
     }
+
+    cout << "Account temporarily locked due to too many failed login attempts."
+         << endl;
 
     return 0;
 }
