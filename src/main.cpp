19b36454e2f92a6402faf9d8fb32775989745fce
Postmanchad <118473087+Postmanchad@users.noreply.github.com>
2026-05-09 21:20:16 -0400

    Add menu options and user choice handling

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,49 @@
 using namespace std;
 
 int main() {
-    cout << "Welcome to CyberQuest!" << endl;
-    cout << "Cybersecurity Training Game Prototype" << endl;
+
+    int choice;
+
+    cout << "==== Welcome to CyberQuest ====" << endl;
+
+    cout << "\n1. Login" << endl;
+    cout << "2. Password Security Challenge" << endl;
+    cout << "3. Phishing Detection Challenge" << endl;
+    cout << "4. Encryption Basics Challenge" << endl;
+    cout << "5. View Scores" << endl;
+    cout << "6. Exit" << endl;
+
+    cout << "\nEnter your choice: ";
+    cin >> choice;
+
+    if(choice == 1) {
+
+        cout << "\nOpening Login System..." << endl;
+    }
+    else if(choice == 2) {
+
+        cout << "\nOpening Password Security Challenge..." << endl;
+    }
+    else if(choice == 3) {
+
+        cout << "\nOpening Phishing Detection Challenge..." << endl;
+    }
+    else if(choice == 4) {
+
+        cout << "\nOpening Encryption Basics Challenge..." << endl;
+    }
+    else if(choice == 5) {
+
+        cout << "\nOpening Score Tracker..." << endl;
+    }
+    else if(choice == 6) {
+
+        cout << "\nExiting CyberQuest..." << endl;
+    }
+    else {
+
+        cout << "\nInvalid choice." << endl;
+    }
 
     return 0;
 }
