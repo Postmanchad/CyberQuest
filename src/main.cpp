68ca5909e16ef671a40c209b33bfa85176c0738d
Postmanchad <118473087+Postmanchad@users.noreply.github.com>
2026-05-09 20:13:54 -0400

    Add main.cpp for Cybersecurity Training Game
    Add initial main.cpp prototype

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Welcome to CyberQuest!" << endl;
+    cout << "Cybersecurity Training Game Prototype" << endl;
+
+    return 0;
+}
